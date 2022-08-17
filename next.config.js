ef53c9555d7c249dbe7db4a466f18401cf617eae
Mingu Kim <mingu757377@gmail.com>
2022-08-18 02:17:54 +0900

    Next Movie

--- next.config.js
+++ next.config.js
@@ -1,7 +1,30 @@
+const API_KEY = process.env.API_KEY
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/old/:path*',
+        destination: '/new/:path*',
+        permanent: false,
+      },
+    ]
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/movies',
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+      {
+        source: '/api/movie/:id',
+        destination: `https://api.themoviedb.org/3/movie/:id?api_key=${API_KEY}`,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
